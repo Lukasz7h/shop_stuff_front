98e42d72d439f4e95e7124a5b615d539cf01e1b1
Lukasz <luuukasz368@gmail.com>
2023-08-03 14:38:30 +0200

    update basket details component
    styles in this component have been changed

--- src/app/main/stuffs/basket/basket-details/basket-details.component.ts
+++ src/app/main/stuffs/basket/basket-details/basket-details.component.ts
@@ -45,7 +45,7 @@ export class BasketDetailsComponent implements OnInit, AfterViewInit
             y: Math.random() * H, //y-coordinate
             r: Math.random() * 15 + 1, //radius
             d: Math.random() * mp, //density
-            color: "rgba(255, 255, 255)",
+            color: "#4387ec",
             tilt: Math.floor(Math.random() * 5) - 5
         });
     }
@@ -57,35 +57,30 @@ export class BasketDetailsComponent implements OnInit, AfterViewInit
         for (var i = 0; i < mp; i++) {
             var p = particles[i];
             ctx.beginPath();
+
             ctx.lineWidth = p.r;
-            ctx.strokeStyle = p.color; // Green path
+            ctx.strokeStyle = p.color;
+
             ctx.moveTo(p.x, p.y);
             ctx.lineTo(p.x + p.tilt + p.r / 3, p.y + p.tilt);
-            ctx.stroke(); // Draw it
+            ctx.stroke();
         }
 
         update();
     }
 
-    //Function to move the snowflakes
-    //angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
     var angle = 0;
 
     function update() {
         angle += 0.001;
         for (var i = 0; i < mp; i++) {
             var p = particles[i];
-            //Updating X and Y coordinates
-            //We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
-            //Every particle has its own density which can be used to make the downward movement different for each flake
-            //Lets make it more random by adding in the radius
+
             p.y += Math.cos(angle + p.d) + 1 + p.r / 20;
             p.x += Math.sin(angle) * 2;
 
-            //Sending flakes back from the top when it exits
-            //Lets make it a bit more organic and let flakes enter from the left and right also.
             if (p.x > W + 5 || p.x < -5 || p.y > H) {
-                if (i % 3 > 0) //66.67% of the flakes
+                if (i % 3 > 0)
                 {
                     particles[i] = {
                         x: Math.random() * W,
@@ -96,9 +91,7 @@ export class BasketDetailsComponent implements OnInit, AfterViewInit
                         tilt: p.tilt
                     };
                 } else {
-                    //If the flake is exitting from the right
                     if (Math.sin(angle) > 0) {
-                        //Enter from the left
                         particles[i] = {
                             x: -5,
                             y: Math.random() * H,
@@ -107,8 +100,8 @@ export class BasketDetailsComponent implements OnInit, AfterViewInit
                             color: p.color,
                             tilt: p.tilt
                         };
-                    } else {
-                        //Enter from the right
+                    }
+                    else {
                         particles[i] = {
                             x: W + 5,
                             y: Math.random() * H,
@@ -123,7 +116,6 @@ export class BasketDetailsComponent implements OnInit, AfterViewInit
         }
     }
 
-    //animation loop
     setInterval(draw, 30);
   }
 
