518d9b74f54d2679e4eaf598104e2364ec80a49b
Lukasz <luuukasz368@gmail.com>
2023-08-09 16:39:07 +0200

    upd

--- src/app/main/stuffs/stuffs.component.ts
+++ src/app/main/stuffs/stuffs.component.ts
@@ -25,13 +25,26 @@ export class StuffsComponent implements OnInit
   nameInput = new Subject();
   lastNumber: number;
 
-  getLogo(name: string, id: number): string
-  {
-    console.log(name);
-
-    this.lastNumber = id;
-    return logos[`${name}`];
-  }
+  logos = {
+    Abc: 'assets/images/abc.png',
+    Aldi: 'assets/images/aldi.svg.png',
+    Auchan: 'assets/images/auchan.png',
+    Biedronka: 'assets/images/biedronka.jpg',
+    Carrefour: 'assets/images/Carrefour_logo.svg.png',
+    Delikatesy: 'assets/images/delikatesy-centrum.svg',
+    Dino: 'assets/images/dino.png',
+    'E.leclerc': 'assets/images/E.Leclerc.svg.png',
+    Groszek: 'assets/images/dino.png',
+    Intermarche: 'assets/images/intermarche.svg.png',
+    Kaufland: 'assets/images/kaufland.png',
+    Lewiatan: 'assets/images/lewiatan.svg',
+    Lidl: 'assets/images/lidl.png',
+    Netto: 'assets/images/netto.png',
+    'Piotr i Paweł': 'assets/images/Piotr_i_Paweł.png',
+    Stokrotka: 'assets/images/stokrotka.png',
+    Tesco: 'assets/images/tesco.jpg',
+    'Żabka': 'assets/images/żabka.svg.png'
+  };
 
   constructor(
     private httpClient: HttpClient,
