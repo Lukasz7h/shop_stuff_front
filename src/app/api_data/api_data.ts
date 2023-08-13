5b0c62406ffb835c85a5d29844d78323d39dad59
Lukasz <luuukasz368@gmail.com>
2023-08-13 23:14:03 +0200

    up

--- src/app/api_data/api_data.ts
+++ src/app/api_data/api_data.ts
@@ -1,4 +1,4 @@
 export const data = {
     url: "https://shopback-production-1cf1.up.railway.app/",
-    socket: "ws://localhost:8080/"
+    socket: "ws://shopback-production-1cf1.up.railway.app:3088/"
 }
