72f92f5be9a4d4e0dfdee827c23dab721aca5f81
Lukasz <luuukasz368@gmail.com>
2023-08-14 15:37:26 +0200

    upd

--- src/app/main/stuffs/details/details.service.ts
+++ src/app/main/stuffs/details/details.service.ts
@@ -237,11 +237,7 @@ export class DetailsService {
       back = period;
       period = sentence == 'back'? period - 1: period + 1;
 
-      console.log(this.currentPeriod)
-
-      console.log(this[`storesHistory${this.currentPeriod}`][period]);
-      console.log(this[`storesHistory${this.currentPeriod}`]);
-      console.log(period);
+      if(!this.currentPeriod) this.currentPeriod = "Month";
 
       type == "bar"?
       data = this[`storesHistory${this.currentPeriod}`][period]: [this.lineChartLabels = labels, data = particularData];
